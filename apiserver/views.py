3908db57222494c79c2c57cbb2afc56bc21bba3d
dragonhail <dragonhailstone@gmail.com>
2024-10-21 14:42:21 +0900

    test2

--- apiserver/views.py
+++ apiserver/views.py
@@ -4,5 +4,5 @@ from rest_framework import status
 
 @api_view(['GET'])
 def index(request):
-    data = {"result": "success", "data":[{"id":"itstudy", "name":"adam"}]}
+    data = {"result": "success", "data":[{"id":"itstudy", "name":"park", "weight":"30"}]}
     return Response(data, status=status.HTTP_200_OK)
